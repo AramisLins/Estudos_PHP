ec66c05d03baa99be093fcec1c16fe1a4c47660c
AramisLins <122841235+AramisLins@users.noreply.github.com>
2023-08-24 09:49:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-echo "Hello Word";
+echo '<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina html gerada pelo php</title>
+</head>
+<body>
+    <h1>batman</h1>
+    <p>em todos os mundos</p>
+    <h3>sou mais o homem de ferro</h3>
+</body>
+</html>';
 
 ?>

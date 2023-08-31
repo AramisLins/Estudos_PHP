479d069d2d203e42ad30fe9e477d431db5fda477
AramisLins <122841235+AramisLins@users.noreply.github.com>
2023-08-31 10:19:19 -0300

    Add files via upload

---
+++ aula 31/teste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="script.php" defer></script>
+</head>
+<body>
+    <h1><?php 
+
+    $nome = $_REQUEST["nome"];
+    echo $nome;
+    ?> lindo</h1>
+</body>
+</html> 
